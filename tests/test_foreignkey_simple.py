3021522c2cde14b2dde6cc955bee4daf4776d3ed
ppoteralski <piotr.poteralski@rst-it.com>
2017-09-06 22:47:49 +0200

    test simple

---
+++ tests/test_foreignkey_simple.py
@@ -0,0 +1,31 @@
+from collections import namedtuple
+from unittest import TestCase
+
+from examples.profile import models
+
+
+class OneForeignKeyTestCase(TestCase):
+    def create_entities(self):
+        self.person = models.Person()
+        self.address = models.Address()
+
+        self.address.person = self.person
+
+        models.db.session.add(self.address)
+        models.db.session.commit()
+
+    def setUp(self):
+        app = namedtuple("App", ['config'])({"SQLALCHEMY_DATABASE_URI": 'sqlite:///:memory:'})
+        models.db.init_app(app)
+        models.db.metadata.create_all()
+
+        self.create_entities()
+
+    def test_relation_created(self):
+        assert self.address.person == self.person
+
+    def test_column_created(self):
+        assert self.address.person_id == self.person.id
+
+    def test_backref_created(self):
+        assert self.person.adresses == [self.address]
