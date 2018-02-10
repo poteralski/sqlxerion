94d2dba257058b850f411675efc5a1abbcc26d26
錦蛇 <poteralski.dev@gmail.com>
2018-02-11 00:23:08 +0100

    yields

--- xerion/meta.py
+++ xerion/meta.py
@@ -5,15 +5,13 @@ from . import fields, relationships, utils, factories
 class XerionMeta(declarative.DeclarativeMeta):
     def __init__(cls, classname, bases, dict_):
         is_abstract = dict_.get('__abstract__', False)
-        new_attrs = cls.create_from_base(dict_, is_abstract)
+        new_attrs = dict(cls.create_from_base(dict_, is_abstract))
         for key, instance in new_attrs.items():
             dict_[key] = instance
             setattr(cls, key, dict_[key])
         super(XerionMeta, cls).__init__(classname, bases, dict_)
 
     def create_from_base(cls, attr_dict, is_abstract):
-        new_attrs = dict()
-
         for attr_name, instance in attr_dict.items():
 
             if isinstance(instance, relationships.ManyToMany):
@@ -37,7 +35,7 @@ class XerionMeta(declarative.DeclarativeMeta):
                         )
                     )
 
-                new_attrs[attr_name] = factories.many_to_many_factory(
+                yield attr_name, factories.many_to_many_factory(
                     cls,
                     instance,
                     secondary,
@@ -46,23 +44,21 @@ class XerionMeta(declarative.DeclarativeMeta):
                 )
 
             elif isinstance(instance, relationships.ForeignKey):
-                new_attrs[attr_name] = factories.foreign_key_rel_factory(
+                yield attr_name, factories.foreign_key_rel_factory(
                     instance.model,
                     instance.nullable, instance.primary_key,
                     instance.extra
                 )
-                new_attrs[
-                    f'{attr_name}_id'] = factories.foreign_key_column_factory(
+                yield f'{attr_name}_id', factories.foreign_key_column_factory(
                     instance.model,
                     instance.nullable, instance.primary_key,
                     instance.extra
                 )
 
             elif isinstance(instance, fields.Field):
-                new_attrs[attr_name] = factories.fields_factory(
+                yield attr_name, factories.fields_factory(
                     instance.column_class,
                     instance.args,
                     instance.kwargs
                 )
 
-        return new_attrs
