4c90682b287d83b5a8fa231ef59aab276aff090c
錦蛇 <poteralski.dev@gmail.com>
2018-02-10 21:58:40 +0100

    style refactor

--- xerion/meta.py
+++ xerion/meta.py
@@ -26,51 +26,60 @@ class XerionMeta(declarative.DeclarativeMeta):
                         secondary=association_table or sqla.Table(
                             f'{self.__tablename__}_{key}',
                             self.metadata,
-                            sqla.Column('left_id', sqla.Integer,
-                                        sqla.ForeignKey(
-                                            f'{self.__tablename__}.id')),
-                            sqla.Column('right_id', sqla.Integer,
-                                        sqla.ForeignKey(
-                                            f'{instance_table_name}.id')),
-                            sqla.PrimaryKeyConstraint('left_id', 'right_id',
-                                                      name=f'{self.__tablename__}_'
-                                                           f'{key}'
-                                                           f'_association_pk')
+                            sqla.Column(
+                                'left_id', sqla.Integer,
+                                sqla.ForeignKey(f'{self.__tablename__}.id')),
+                            sqla.Column(
+                                'right_id',
+                                sqla.Integer,
+                                sqla.ForeignKey(f'{instance_table_name}.id')
+                            ),
+                            sqla.PrimaryKeyConstraint(
+                                'left_id',
+                                'right_id',
+                                name=f'{self.__tablename__}_{key}_association_pk'
+                            )
                         ),
                         **instance.extra
                     )
-
-                # Problem with Many To Many auto backrefs relationship
-                # https://stackoverflow.com/questions/45313491/
 
                 if not is_abstract:
                     model = get_model(cls, instance.model)
                     secondary = f'{cls.__tablename__}_{key}'
                     attr_name = instance.extra.pop('backref', cls.__tablename__)
-                    setattr(model, attr_name,
-                            sqla_orm.relationship(cls, secondary=secondary))
+                    setattr(
+                        model,
+                        attr_name,
+                        sqla_orm.relationship(cls, secondary=secondary)
+                    )
 
                 new_attrs[key] = declarative.declared_attr(get_relationship)
 
             elif isinstance(instance, relationships.ForeignKey):
 
                 def get_column(self, instance=instance):
-                    instance_table_name = get_model(self,
-                                                    instance.model).__tablename__
-                    return sqla.Column(sqla.Integer, sqla.ForeignKey(
-                        f'{instance_table_name}.id'),
-                                       nullable=instance.nullable,
-                                       primary_key=instance.primary_key)
+                    instance_model = get_model(self, instance.model)
+                    instance_table_name = instance_model.__tablename__
+                    return sqla.Column(
+                        sqla.Integer,
+                        sqla.ForeignKey(f'{instance_table_name}.id'),
+                        nullable=instance.nullable,
+                        primary_key=instance.primary_key
+                    )
 
                 new_attrs[f'{key}_id'] = declarative.declared_attr(get_column)
                 new_attrs[key] = declarative.declared_attr(
                     lambda self, instance=instance: sqla_orm.relationship(
                         instance.model,
-                        **instance.extra))
+                        **instance.extra
+                    )
+                )
 
             elif isinstance(instance, fields.Field):
                 new_attrs[key] = sqla.Column(
-                    instance.column_class(*instance.args), **instance.kwargs)
+                    instance.column_class(*instance.args),
+                    **instance.kwargs
+                )
 
         for key, instance in new_attrs.items():
             dict_[key] = instance
